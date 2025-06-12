522fed738a563b4d3c50ac006eee674af3ab84fa
Asir Hameem <asirhameem@gmail.com>
2025-06-13 02:56:40 +0600

    feat: added dynamic counts

--- src/pages/legal/PrivacyPolicyPage.tsx
+++ src/pages/legal/PrivacyPolicyPage.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
-import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const PrivacyPolicyPage = () => {
